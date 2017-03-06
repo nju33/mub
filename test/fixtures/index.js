9493678ffd2e6c66e19c1579f90a25e42241dd70
nju33 <nju33.ki@gmail.com>
2017-03-06 19:14:21 +0900

    Commit

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,10 +1,15 @@
 (function () {
-  const mub = new Mub('root');
-  mub.setData({
-    header: ['aaa'],
-    rows: [
-      ['bbbb'],
-      ['cccc']
-    ]
+  const mub = new Mub({
+    target: document.getElementById('root')
+  });
+  mub.set({
+    data: {
+      header: ['aaa', 'bbb', 'ccc'],
+      rows: [
+        ['bbbb', 'alksdjflksjdflskdjflj sldkfak sdjlfa sjdfl sdf sdf', 'foo'],
+        ['cccc', 'asdlfjasldkfjk lasjdfljs ldkjfalsj dfjsldfjlsa jdflajsdj fsldjfksdjflsdjflskdjf ', 'bar'],
+        ['alsdkjfl', 'sjdfljsdfjl asjdfkj lsdjfl sjadlf jsdlkfj lsdjfl sjdfj sldjfls jdfjsd jflsdf ', 'baz']
+      ]
+    }
   });
 })();
